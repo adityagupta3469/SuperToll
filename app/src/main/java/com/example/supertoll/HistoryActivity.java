e90f96d35d114fa1b89f7528e6a59698dddb4d9b
aditya gupta <adityagupta300498@gmail.com>
2020-07-10 02:53:58 +0530

    Program of project

---
+++ app/src/main/java/com/example/supertoll/HistoryActivity.java
@@ -0,0 +1,101 @@
+package com.example.supertoll;
+
+import android.os.Bundle;
+import android.text.format.DateFormat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.example.supertoll.historyRecyclerView.HistoryAdapter;
+import com.example.supertoll.historyRecyclerView.HistoryObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Objects;
+
+
+public class HistoryActivity extends AppCompatActivity {
+    private String customerOrDriver, userId;
+
+    private RecyclerView.Adapter mHistoryAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        RecyclerView mHistoryRecyclerView = (RecyclerView) findViewById(R.id.historyRecyclerView);
+        mHistoryRecyclerView.setNestedScrollingEnabled(false);
+        mHistoryRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mHistoryLayoutManager = new LinearLayoutManager(HistoryActivity.this);
+        mHistoryRecyclerView.setLayoutManager(mHistoryLayoutManager);
+        mHistoryAdapter = new HistoryAdapter(getDataSetHistory(), HistoryActivity.this);
+        mHistoryRecyclerView.setAdapter(mHistoryAdapter);
+
+
+        customerOrDriver = Objects.requireNonNull(getIntent().getExtras()).getString("customerOrDriver");
+        userId = Objects.requireNonNull(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser())).getUid();
+        getUserHistoryIds();
+    }
+
+    private void getUserHistoryIds() {
+        DatabaseReference userHistoryDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(customerOrDriver).child(userId).child("history");
+        userHistoryDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot history : dataSnapshot.getChildren()){
+                        FetchRideInformation(history.getKey());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void FetchRideInformation(String rideKey) {
+        DatabaseReference historyDatabase = FirebaseDatabase.getInstance().getReference().child("history").child(rideKey);
+        historyDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String rideId = dataSnapshot.getKey();
+                    Long timestamp = 0L;
+                    for(DataSnapshot child : dataSnapshot.getChildren()){
+                        if (child.getKey().equals("timestamp")){
+                            timestamp = Long.valueOf(child.getValue().toString());
+                        }
+                    }
+                    HistoryObject obj = new HistoryObject(rideId, getDate(timestamp));
+                    resultsHistory.add(obj);
+                    mHistoryAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private String getDate(Long time) {
+        Calendar cal = Calendar.getInstance(Locale.getDefault());
+        cal.setTimeInMillis(time*1000);
+        String date = DateFormat.format("MM-dd-yyyy hh:mm", cal).toString();
+        return date;
+    }
+
+    private ArrayList resultsHistory = new ArrayList<HistoryObject>();
+    private ArrayList<HistoryObject> getDataSetHistory() {
+        return resultsHistory;
+    }
+}
